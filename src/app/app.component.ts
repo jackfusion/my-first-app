50b13401ffd12fb2c577cae05c9297be60e093b0
jackfusion <kendewitt@yftg.ca>
2021-09-10 09:09:25 -0500

    server example code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  username: string = '';
+  showSecret: boolean = false;
+  log = [];
+  OnToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
 }
